7550147de2570152b8cb71da943b98006849b41f
Lucas <lucas_fernandomendes@hotmail.com>
2020-08-01 20:22:47 -0300

    merge

--- src/modules/auth/views/Login.vue
+++ src/modules/auth/views/Login.vue
@@ -1,66 +1,64 @@
 <template>
-	<v-container fill-height>
-		<v-layout justify-center align-center>
-			<v-flex md3>
-				<v-card class="elevation-5">
-					<v-toolbar color="teal darken-1" dark>
-						<v-toolbar-title class="text-center">{{ texts.toolbar }}</v-toolbar-title>
-						<v-spacer></v-spacer>
-						<v-progress-circular v-show="isLoading" indeterminate size="35" color="white" />
-					</v-toolbar>
+  <v-container class="mt-5">
+    <v-layout justify-center align-center>
+      <v-flex md3>
+        <h2 class="text-center pa-4">Entrar no MasterWeb</h2>
 
-					<v-card-text>
-						<v-form>
-							<v-text-field
-								v-if="!isLogin"
-								prepend-icon="fas fa-user"
-								:error-messages="nomeErrors"
-								:success="!$v.user.nome.$invalid"
-								v-model.trim="$v.user.nome.$model"
-								label="nome"
-								type="text"
-							/>
-							<v-text-field
-								prepend-icon="fas fa-envelope"
-								label="email"
-								type="email"
-								:error-messages="emailErrors"
-								:success="!$v.user.email.$invalid"
-								v-model.trim="$v.user.email.$model"
-							/>
-							<v-text-field
-								prepend-icon="fas fa-lock"
-								label="senha"
-								type="password"
-								:error-messages="senhaErrors"
-								:success="!$v.user.senha.$invalid"
-								v-model.trim="$v.user.senha.$model"
-							/>
-						</v-form>
+        <v-card class="v-sheet--outlined">
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                v-if="!isLogin"
+                prepend-icon="fas fa-user"
+                :error-messages="nomeErrors"
+                :success="!$v.user.nome.$invalid"
+                v-model.trim="$v.user.nome.$model"
+                label="nome"
+                type="text"
+              />
+              <v-text-field
+                prepend-icon="fas fa-envelope"
+                label="email"
+                type="email"
+                :error-messages="emailErrors"
+                :success="!$v.user.email.$invalid"
+                v-model.trim="$v.user.email.$model"
+              />
+              <v-text-field
+                prepend-icon="fas fa-lock"
+                label="senha"
+                type="password"
+                :error-messages="senhaErrors"
+                :success="!$v.user.senha.$invalid"
+                v-model.trim="$v.user.senha.$model"
+              />
+            </v-form>
 
-						<!-- <v-btn @click="isLogin = !isLogin" block text class="mt-3">{{ texts.button }}</v-btn> -->
-					</v-card-text>
+            <v-btn @click="isLogin = !isLogin" block text class="mt-3">{{ texts.button }}</v-btn>
 
-					<v-card-actions>
-						<v-spacer />
-						<v-btn
-							:disabled="$v.$invalid"
-							@click="submit"
-							color="white--text teal darken-1"
-							large
-						>{{ texts.toolbar }}</v-btn>
-					</v-card-actions>
+            <v-progress-circular v-show="isLoading" indeterminate size="35" color="teal darken-1" />
+          </v-card-text>
 
-					<v-snackbar v-model="showSnackbar" top>
-						{{error}}
-						<v-btn icon color="pink lighten-1" @click="showSnackbar = false">
-							<v-icon>fas fa-times</v-icon>
-						</v-btn>
-					</v-snackbar>
-				</v-card>
-			</v-flex>
-		</v-layout>
-	</v-container>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn
+              :disabled="$v.$invalid"
+              @click="submit"
+              color="white--text teal darken-1"
+              class="v-btn--block"
+            >{{ texts.toolbar }}</v-btn>
+          </v-card-actions>
+
+          <v-snackbar v-model="showSnackbar" top>
+            {{error}}
+            <v-btn icon color="pink lighten-1" @click="showSnackbar = false">
+              <v-icon>fas fa-times</v-icon>
+            </v-btn>
+          </v-snackbar>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -70,104 +68,108 @@ import { formatError } from "@/utils";
 import AuthService from "@/graphql/auth/services/auth-service";
 
 export default {
-	name: "Login",
-	data() {
-		return {
-			user: { nome: null, email: null, senha: null },
-			isLogin: true,
-			isLoading: false,
-			showSnackbar: false,
-			error: null,
-			textError: "Campo Obrigatório"
-		};
-	},
-	validations() {
-		const validations = {
-			user: {
-				email: { required, email },
-				senha: { required, minLength: minLength(6) }
-			}
-		};
+  name: "Login",
+  data() {
+    return {
+      user: { nome: null, email: null, senha: null },
+      isLogin: true,
+      isLoading: false,
+      showSnackbar: false,
+      error: null,
+      textError: "Campo Obrigatório",
+    };
+  },
+  validations() {
+    const validations = {
+      user: {
+        email: { required, email },
+        senha: { required, minLength: minLength(6) },
+      },
+    };
 
-		//nem passa para o proximo, apenas sai da função
-		if (this.isLogin) {
-			return validations;
-		}
+    //nem passa para o proximo, apenas sai da função
+    if (this.isLogin) {
+      return validations;
+    }
 
-		return {
-			user: {
-				...validations.user,
-				nome: { required, minLength: minLength(6) }
-			}
-		};
-	},
-	methods: {
-		async submit() {
-			this.isLoading = true;
-			try {
-				this.isLogin
-					? await AuthService.login(this.user)
-					: await AuthService.signup(this.user);
-				this.$router.push(this.$route.query.redirect || "/dashboard");
-			} catch (error) {
-				this.error = formatError(error.message);
-				this.showSnackbar = true;
-			} finally {
-				this.isLoading = false;
-			}
-		}
-	},
-	computed: {
-		texts() {
-			return this.isLogin
-				? { toolbar: "Login", button: "Criar conta" }
-				: { toolbar: "Criar Conta", button: "Já tenho uma conta" };
-		},
-		emailErrors() {
-			const errors = [];
-			const email = this.$v.user.email;
+    return {
+      user: {
+        ...validations.user,
+        nome: { required, minLength: minLength(6) },
+      },
+    };
+  },
+  methods: {
+    async submit() {
+      this.isLoading = true;
+      try {
+        this.isLogin
+          ? await AuthService.login(this.user)
+          : await AuthService.signup(this.user);
+        this.$router.push(this.$route.query.redirect || "/dashboard");
+      } catch (error) {
+        this.error = formatError(error.message);
+        this.showSnackbar = true;
+      } finally {
+        this.isLoading = false;
+      }
+    },
+  },
+  computed: {
+    texts() {
+      return this.isLogin
+        ? { toolbar: "Login", button: "Criar conta" }
+        : { toolbar: "Criar Conta", button: "Já tenho uma conta" };
+    },
+    emailErrors() {
+      const errors = [];
+      const email = this.$v.user.email;
 
-			if (!email.$dirty) {
-				return errors;
-			}
+      if (!email.$dirty) {
+        return errors;
+      }
 
-			!email.required ? errors.push(this.textError) : null;
-			!email.email ? errors.push(this.textError) : null;
+      !email.required ? errors.push(this.textError) : null;
+      !email.email ? errors.push(this.textError) : null;
 
-			return errors;
-		},
-		senhaErrors() {
-			const errors = [];
-			const senha = this.$v.user.senha;
+      return errors;
+    },
+    senhaErrors() {
+      const errors = [];
+      const senha = this.$v.user.senha;
 
-			if (!senha.$dirty) {
-				return errors;
-			}
+      if (!senha.$dirty) {
+        return errors;
+      }
 
-			!senha.required ? errors.push(this.textError) : null;
-			!senha.minLength
-				? errors.push(`Mínimo ${senha.$params.minLength.min} caracteres`)
-				: null;
+      !senha.required ? errors.push(this.textError) : null;
+      !senha.minLength
+        ? errors.push(`Mínimo ${senha.$params.minLength.min} caracteres`)
+        : null;
 
-			return errors;
-		},
-		nomeErrors() {
-			const errors = [];
-			const nome = this.$v.user.nome;
+      return errors;
+    },
+    nomeErrors() {
+      const errors = [];
+      const nome = this.$v.user.nome;
 
-			if (!nome.$dirty) {
-				return errors;
-			}
+      if (!nome.$dirty) {
+        return errors;
+      }
 
-			!nome.required ? errors.push(this.textError) : null;
-			!nome.minLength
-				? errors.push(`Mínimo ${nome.$params.minLength.min} caracteres`)
-				: null;
+      !nome.required ? errors.push(this.textError) : null;
+      !nome.minLength
+        ? errors.push(`Mínimo ${nome.$params.minLength.min} caracteres`)
+        : null;
 
-			return errors;
-		}
-	}
+      return errors;
+    },
+  },
 };
 </script>
 
-
+<style>
+main {
+  background-color: #f9f9f9;
+}
+</style>
